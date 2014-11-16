918adf3ef5581a62c2f850489e4ade4d0a45035b
Tobias Preuss <tobias.preuss@googlemail.com>
2014-11-16 10:38:21 +0100

    Improve formatting - no code change here.

--- trees.sql
+++ trees.sql
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE tree (
-	tree_id serial PRIMARY KEY, 
+	tree_id serial PRIMARY KEY,
 	location geometry,
 	created timestamp with time zone DEFAULT now(),
 	CONSTRAINT tree_geom_uniq UNIQUE (location)
@@ -9,12 +9,15 @@ CREATE TABLE tree (
 
 
 CREATE TABLE history (
-	history_id serial PRIMARY KEY, 
+	history_id serial PRIMARY KEY,
 	import_time timestamp with time zone DEFAULT now(),
-	tree_id integer NULL, 
+	tree_id integer NULL,
 	value integer NOT NULL,
-	CONSTRAINT history_tree_id_fkey FOREIGN KEY (tree_id) REFERENCES tree (tree_id) MATCH SIMPLE ON UPDATE RESTRICT ON DELETE RESTRICT
-); 
+	CONSTRAINT history_tree_id_fkey FOREIGN KEY (tree_id)
+	REFERENCES tree (tree_id) MATCH SIMPLE
+	ON UPDATE RESTRICT
+	ON DELETE RESTRICT
+);
 
 
 # Look up or generate foreign key
@@ -23,9 +26,14 @@ CREATE FUNCTION get_tree(new_location geometry) RETURNS integer AS $$
 DECLARE
 	result_tree_id integer;
 BEGIN
-	SELECT tree_id FROM tree WHERE tree.location = new_location LIMIT 1 INTO result_tree_id;
+	SELECT tree_id
+	FROM tree
+	WHERE tree.location = new_location
+	LIMIT 1 INTO result_tree_id;
 	IF NOT FOUND THEN
-		INSERT INTO tree (location) VALUES (new_location) RETURNING tree_id INTO result_tree_id;
+		INSERT INTO tree (location)
+		VALUES (new_location)
+		RETURNING tree_id INTO result_tree_id;
 	END IF;
 	RETURN result_tree_id;
 END
@@ -35,11 +43,29 @@ LANGUAGE plpgsql;
 
 # Create view combining both tables to execute INSERTs on
 
-CREATE VIEW tree_history AS SELECT history.history_id, history.tree_id, tree.location, history.value FROM tree JOIN history ON tree.tree_id = history.tree_id;
+CREATE VIEW tree_history AS
+	SELECT
+		history.history_id,
+		history.tree_id,
+		tree.location,
+		history.value
+	FROM tree
+	JOIN history
+	ON tree.tree_id = history.tree_id;
 
 # Create INSERT rule for redirect
 
-CREATE RULE tree_history_insert AS ON INSERT TO tree_history DO INSTEAD INSERT INTO history (tree_id, value) VALUES (get_tree(NEW.location), NEW.value);
+CREATE RULE tree_history_insert AS
+	ON INSERT TO tree_history
+	DO INSTEAD
+		INSERT INTO history (
+			tree_id,
+			value
+		)
+		VALUES (
+			get_tree(NEW.location),
+			NEW.value
+		);
 
 # TODO: RULES for UPDATE and DELETE
 
